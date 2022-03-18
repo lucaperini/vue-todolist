8f0252dcb0e4ab9acdb60522c92e56ba373973ec
lucaperini <luca.perini@gmail.com>
2022-03-18 16:40:09 +0100

    milestone 1 e milestone 2

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const app = new Vue (
+
+    {
+    
+    el : "#app",
+
+    data: { 
+        todoList: [
+            {
+                text: "comprare fave di fuca",
+                done: false,
+            },
+            {
+                text: "lavare la testa al capo",
+                done: true,
+            },
+            {
+                text: "fare la barba al criceto",
+                done: false,
+            },
+            {
+                text: "rimbrottare un cactus",
+                done: true,
+            },
+            {
+                text: "spostare una candela",
+                done: false,
+            },
+            {
+                text: "imbiancare una parete senza stanza",
+                done: true,
+            },
+        ]
+    },
+    
+    methods: {
+
+    },
+});
